889c3cb4cff668ce64487d645bc37767da2b6e5d
gabriel d. karpman <gdkar@alum.mit.edu>
2017-01-14 07:49:51 -0500

    use forward transform in both directions

--- src/TimeDerivativeWindow.hpp
+++ src/TimeDerivativeWindow.hpp
@@ -14,15 +14,15 @@ OutputIt time_derivative_window(InputIt wbegin, InputIt wend, OutputIt dst)
     {
         auto base_idx = n/2 + 1;
         auto base_mul = - ( n - 1 ) * 0.5f;
-        auto norm_mul = float(2 * M_PI) / n;
+        auto norm_mul = float(2 * M_PI) / (n * n);
         for(auto i = 0; i < n; ++i) {
             auto idx = ( i + base_idx ) % n;
-            auto mul = (i + base_mul) * norm_mul;
-            freq_r[idx] *= -mul;
-            freq_i[idx] *= -mul;
+            auto mul =-(i + base_mul) * norm_mul;
+            time_r[idx] = freq_i[idx]*mul;
+            time_i[idx] = freq_r[idx]*mul;
         }
     }
-    fft.inverse(time_r.begin(),time_i.begin(),freq_r.cbegin(),freq_i.cbegin());
-    return std::copy(time_i.cbegin(),time_i.cend(),dst);
+    fft.forward(freq_r.begin(),freq_i.begin(),time_r.begin(),time_i.begin());
+    return std::copy(freq_r.cbegin(),freq_r.cend(),dst);
 }
 }
