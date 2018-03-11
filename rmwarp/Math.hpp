61e5fa585c6f2b572614b670e8f3e054ec58ff0a
gdkar <gdkar@alum.mit.edu>
2018-03-10 19:58:42 -0500

    remove lerp, unused and commented things

--- rmwarp/Math.hpp
+++ rmwarp/Math.hpp
@@ -18,38 +18,6 @@ template<class T> constexpr const T T_PI = T{M_PI};
 template<class T> constexpr const T T_TWO_PI = T{2*M_PI};
 template<class T> constexpr const T T_PI_2 = T{0.5*M_PI};
 
-//template<class T>
-//constexpr decltype(auto) princarg(T a) { return bs::pedantic_(bs::rem)(a,bs::Twopi<T>());}
-
-/*
-template<>
-constexpr int popcount(uint32_t t) { return __builtin_popcount(t); }
-template<>
-constexpr int popcount(uint64_t t) { return __builtin_popcountl(t); }
-template<>
-constexpr int popcount(uint16_t t) { return popcount(uint32_t(t)); }
-template<>
-constexpr int popcount(uint8_t t)  { return popcount(uint32_t(t)); }
-template<>
-constexpr int popcount(int32_t t)  { return __builtin_popcount(t); }
-template<>
-constexpr int popcount(int64_t t)  { return __builtin_popcountl(t); }
-template<>
-constexpr int popcount(int16_t t)  { return popcount(uint32_t(t)); }
-template<>
-constexpr int popcount(int8_t t)   { return popcount(uint32_t(t)); }
-*/
-template<typename T>
-constexpr std::enable_if_t<std::is_integral<T>::value,T>
-roundup ( T x)
-{
-    using U = typename std::make_unsigned<T>::type;
-    auto u = U{x} - U{1};
-    for(auto j = 1; j < std::numeric_limits<U>::digits(); j <<= 1)
-        u |= (u >> j);
-    return T{u + U{1}};
-}
-
 template<class T>
 constexpr T align_up(T x, T a) {
     using U = std::make_unsigned_t<T>;
@@ -70,21 +38,5 @@ template<class T>
 constexpr const T &clamp( const T& v, const T& lo, const T& hi)
 {
     return clamp( v, lo, hi, std::less<>{});
-}
-template<class T, class R>
-constexpr std::enable_if_t<std::is_floating_point<R>::value,T> lerp(T lo, T hi, R _frac, R _range)
-{
-    return lo + (hi - lo) * (_frac * (R{1}/_range));
-}
-template<class T, class R>
-constexpr std::enable_if_t<!std::is_floating_point<R>::value,T> lerp(T lo, T hi, R _frac, R _range)
-{
-    return lo + (((hi - lo) * _frac)/_range);
-}
-template<class T, class R, R _range = R{1}>
-constexpr std::enable_if_t<std::is_floating_point<R>::value, T> lerp(T lo, T hi, R _frac)
-{
-    constexpr auto _factor = R{1}/_range;
-    return lo + (hi - lo) * (_frac*_factor);
 }
 }
